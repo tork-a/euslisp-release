502fddfabb05d0abd041309f8cc4dab94d637357
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-02-19 19:50:24 +0900

    Rebase from 'upstream'

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,11 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 function travis_time_start {
-    TRAVIS_START_TIME=$(date +%s)
+    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+        TRAVIS_START_TIME=$(( $(date +%s)*1000000000 ))
+    else
+        TRAVIS_START_TIME=$(date +%s%N)
+    fi
     TRAVIS_TIME_ID=$(cat /dev/urandom | LC_ALL=C LC_CTYPE=C tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
     TRAVIS_FOLD_NAME=$1
     echo -e "\e[0Ktravis_fold:start:$TRAVIS_FOLD_NAME"
@@ -14,7 +18,11 @@ function travis_time_start {
 function travis_time_end {
     set +x # disable debug information
     _COLOR=${1:-32}
-    TRAVIS_END_TIME=$(date +%s)
+    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+        TRAVIS_END_TIME=$(( $(date +%s)*1000000000 ))
+    else
+        TRAVIS_END_TIME=$(date +%s%N)
+    fi
     TIME_ELAPSED_SECONDS=$(( ($TRAVIS_END_TIME - $TRAVIS_START_TIME)/1000000000 ))
     echo -e "travis_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n\e[0K"
     echo -e "travis_fold:end:$TRAVIS_FOLD_NAME"
@@ -28,7 +36,9 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     travis_time_end
 
     travis_time_start setup.apt-get_install
-    ret=1; while [ $ret != 0 ]; do sudo apt-get install -qq -y git make gcc g++ libjpeg-dev libxext-dev libx11-dev libgl1-mesa-dev libglu1-mesa-dev libpq-dev libpng-dev xfonts-100dpi xfonts-75dpi && ret=0 || echo "failed, retry"; done # msttcorefonts could not install on 14.04 travis
+    ret=1; while [ $ret != 0 ]; do sudo apt-get install -qq -y git make gcc g++ libjpeg-dev libxext-dev libx11-dev libgl1-mesa-dev libglu1-mesa-dev libpq-dev libpng-dev xfonts-100dpi xfonts-75dpi pkg-config libbullet-dev && ret=0 || echo "failed, retry"; done # msttcorefonts could not install on 14.04 travis
+    # unset protocol version https://github.com/juju/charm-tools/issues/532
+    git config --global --unset protocol.version || echo "OK"
     if [ "`uname -m`" == "x86_64" ] ; then sudo apt-get install -qq -y texlive-latex-base ptex-bin latex2html nkf poppler-utils || echo "ok"; fi # 16.04 does ont have ptex bin
     travis_time_end
 
@@ -38,16 +48,76 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     # skip if already installed
     # https://discourse.brew.sh/t/skip-ignore-brew-install-if-package-is-already-installed/633/2
     # brew install jpeg libpng mesalib-glw;
-    brew list jpeg &>/dev/null || brew install jpeg
-    brew list libpng &>/dev/null || brew install libpng
-    brew list mesalib-glw &>/dev/null || brew install mesalib-glw
+    # use HOMEBREW_NO_AUT_UPDATE to fix unexpected keyword error https://travis-ci.community/t/syntax-error-unexpected-keyword-rescue-expecting-keyword-end-in-homebrew/5623
+    brew list jpeg &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install jpeg
+    brew list libpng &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install libpng
+    brew list mesalib-glw &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install mesalib-glw
+    brew list bullet &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install bullet
     travis_time_end
 
 fi
 
+### for multiarch compile test
+if [ "$QEMU" != "" ]; then
+    travis_time_start install.dpkg-dev
+    apt-get install -qq -y dpkg-dev
+    travis_time_end
+
+    echo "uname -a : $(uname -a)"
+    echo "uname -m : $(uname -m)"
+    echo "gcc -dumpmachine : $(gcc -dumpmachine)"
+    echo "gcc -dumpversion : $(gcc -dumpversion)"
+    echo "getconf LONG_BIT : $(getconf LONG_BIT)"
+
+    travis_time_start compile.euslisp
+    export EUSDIR=`pwd`
+    eval "$(dpkg-buildflags --export=sh)"
+    make -C lisp -f Makefile.Linux  eus0 eus1 eus2 eusg eusx eusgl eus eusjpeg
+    travis_time_end
+
+    if [[ `gcc -dumpmachine | egrep "^(arm|aarch)"` != "" ]]; then
+        export ARCHDIR=LinuxARM
+    elif [[ `gcc -dumpmachine | egrep "^x86_64"` != "" ]]; then
+        export ARCHDIR=Linux64
+    else
+        export ARCHDIR=Linux
+    fi
+    export PATH=`pwd`/$ARCHDIR/bin:$PATH
+
+    export EXIT_STATUS=0;
+    set +e
+    # run test in EusLisp/test
+    for test_l in test/*.l; do
+
+        travis_time_start euslisp.${test_l##*/}.test
+
+        sed -i 's/\(i-max\ [0-9]000\)0*/\1/' $test_l
+
+        eusgl $test_l;
+        export TMP_EXIT_STATUS=$?
+
+        travis_time_end `expr 32 - $TMP_EXIT_STATUS`
+
+        export EXIT_STATUS=`expr $TMP_EXIT_STATUS + $EXIT_STATUS`;
+    done;
+    echo "Exit status : $EXIT_STATUS";
+
+    travis_time_start euslisp.eusjpeg.test
+
+    eusgl '(progn (load (format nil "~A/lisp/image/jpeg/eusjpeg.l" *eusdir*))(image::write-jpeg-file "test.jpg" (instance color-image24 :init 100 100)) (print *user*) (unix::exit))'
+
+    export TMP_EXIT_STATUS=$?
+
+    travis_time_end `expr 32 - $TMP_EXIT_STATUS`
+    export EXIT_STATUS=`expr $TMP_EXIT_STATUS + $EXIT_STATUS`;
+    echo "Exit status : $EXIT_STATUS";
+    [ $EXIT_STATUS == 0 ] || exit 1
+    exit 0
+fi
+
 travis_time_start install # Use this to install any prerequisites or dependencies necessary to run your build
 cd ${HOME}
-[ -e jskeus ] || git clone http://github.com/euslisp/jskeus jskeus
+[ -e jskeus ] || git clone --depth 1 http://github.com/euslisp/jskeus jskeus
 ln -s $CI_SOURCE_PATH jskeus/eus
 ln -s `pwd`/jskeus/irteus   jskeus/eus/irteus
 travis_time_end
@@ -60,6 +130,13 @@ else
     make eus-installed WFLAGS="-Werror=implicit-int -Werror=implicit-function-declaration -Werror=incompatible-pointer-types -Werror=int-conversion -Werror=unused-result"
 fi
 make
+
+travis_time_start script.eustag
+
+(cd eus/lisp/tool; make)
+
+travis_time_end
+
 
 travis_time_end
 
@@ -144,6 +221,8 @@ fi
 
         travis_time_end `expr 32 - $TMP_EXIT_STATUS`
 
+        if [[ $TMP_EXIT_STATUS != 0 ]]; then echo "Failed running $test_l. Exiting with $TMP_EXIT_STATUS"; fi
+
         export EXIT_STATUS=`expr $TMP_EXIT_STATUS + $EXIT_STATUS`;
     done;
     echo "Exit status : $EXIT_STATUS";
@@ -151,17 +230,25 @@ fi
 
     # run test in compiled EusLisp/test
     for test_l in $CI_SOURCE_PATH/test/*.l; do
-        # bignum test fails on armhf
-        [[ "`uname -m`" == "arm"* && $test_l =~ bignum.l ]] && continue;
-        # const.l does not compilable https://github.com/euslisp/EusLisp/issues/318
-        [[ $test_l =~ const.l ]] && continue;
 
         travis_time_start compiled.${test_l##*/}.test
 
         eusgl "(let ((o (namestring (merge-pathnames \".o\" \"$test_l\"))) (so (namestring (merge-pathnames \".so\" \"$test_l\")))) (compile-file \"$test_l\" :o o) (if (probe-file so) (load so) (exit 1))))"
         export TMP_EXIT_STATUS=$?
 
-        travis_time_end `expr 32 - $TMP_EXIT_STATUS`
+        export CONTINUE=0
+        # bignum test fails on armhf
+        if [[ "`uname -m`" == "arm"* && $test_l =~ bignum.l ]]; then export CONTINUE=1; fi
+        # sort test fails on armhf  (https://github.com/euslisp/EusLisp/issues/232)
+        if [[ "`uname -m`" == "arm"* && $test_l =~ sort.l ]]; then export CONTINUE=1; fi
+        # const.l does not compilable https://github.com/euslisp/EusLisp/issues/318
+        if [[ $test_l =~ const.l ]]; then export CONTINUE=1; fi
+
+        if [[ $CONTINUE == 0 ]]; then travis_time_end `expr 32 - $TMP_EXIT_STATUS`; else travis_time_end 33; fi
+
+        if [[ $TMP_EXIT_STATUS != 0 ]]; then echo "Failed running $test_l. Exiting with $TMP_EXIT_STATUS"; fi
+
+        if [[ $CONTINUE != 0 ]]; then continue; fi
 
         export EXIT_STATUS=`expr $TMP_EXIT_STATUS + $EXIT_STATUS`;
     done;
@@ -170,14 +257,23 @@ fi
     # run test in jskeus/irteus
     for test_l in irteus/test/*.l; do
 
-        [[ ("`uname -m`" == "arm"* || "`uname -m`" == "aarch"*) && $test_l =~ geo.l|mathtest.l|interpolator.l|test-irt-motion.l|test-pointcloud.l|irteus-demo.l ]] && continue;
-
         travis_time_start irteus.${test_l##*/}.test
 
         irteusgl $test_l;
         export TMP_EXIT_STATUS=$?
 
-        travis_time_end `expr 32 - $TMP_EXIT_STATUS`
+        export CONTINUE=0
+        # irteus-demo.l, robot-model-usage.l and test-irt-motion.l fails on armhf both trusty and xenial
+        if [[ "`uname -m`" == "arm"* && $test_l =~ irteus-demo.l|robot-model-usage.l|test-irt-motion.l ]]; then export CONTINUE=1; fi
+        # skip collision test because bullet of 2.83 or later version is not released in trusty and jessie.
+        # https://github.com/euslisp/jskeus/blob/6cb08aa6c66fa8759591de25b7da68baf76d5f09/irteus/Makefile#L37
+        if [[ ( "$DOCKER_IMAGE" == *"trusty"* || "$DOCKER_IMAGE" == *"jessie"* ) && $test_l =~ test-collision.l ]]; then export CONTINUE=1; fi
+
+        if [[ $CONTINUE == 0 ]]; then travis_time_end `expr 32 - $TMP_EXIT_STATUS`; else travis_time_end 33; fi
+
+        if [[ $TMP_EXIT_STATUS != 0 ]]; then echo "Failed running $test_l. Exiting with $TMP_EXIT_STATUS"; fi
+
+        if [[ $CONTINUE != 0 ]]; then continue; fi
 
         export EXIT_STATUS=`expr $TMP_EXIT_STATUS + $EXIT_STATUS`;
     done;
